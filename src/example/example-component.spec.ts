24707a4068125f35755b314f00570223a65d9520
Josua Pedersen <josua@phntms.com>
2021-11-10 18:21:39 +0000

    Add the example module (#18)

---
+++ src/example/example-component.spec.ts
@@ -0,0 +1,22 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {setupComponentTestingModule} from '../testing/test-lib';
+
+import {PhExampleComponent} from './example-component';
+
+describe('The PhExampleComponent', () => {
+  let component: PhExampleComponent;
+  let fixture: ComponentFixture<PhExampleComponent>;
+
+  beforeEach(() => {
+    setupComponentTestingModule({declarations: [PhExampleComponent]});
+
+    fixture = TestBed.createComponent(PhExampleComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+});
